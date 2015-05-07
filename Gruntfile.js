677be1db3bfa5bca7bea0acccbed83b17453f5f8
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2015-05-07 16:37:12 +0430

    qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,8 +20,11 @@
                 ],
             },
         },
+        qunit: {
+            all: ['<%= dirs.test %>/mx.html']
+        },
         jshint: {
-            files: ['Gruntfile.js', '<%= dirs.source %>/**/*.js', '<%= dirs.test %>/**/*.js'],
+            files: ['Gruntfile.js', '<%= dirs.source %>/**/*.js', '<%= dirs.test %>/unit/**/*.js'],
             options: {
                 boss: true,
                 browser: true,
@@ -43,6 +46,7 @@
                     'define': true,
                     'module': true,
                     'intellisense': true,
+                    'QUnit': true,
                     'mx': true
                 },
             }
@@ -117,11 +121,14 @@
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('dev', ['jshint']);
-    grunt.registerTask('default', ['clean', 'copy', 'jshint', 'uglify']);
+    grunt.registerTask('test', ['qunit']);
+    grunt.registerTask('default', ['dev', 'clean', 'copy', 'uglify']);
+    grunt.registerTask('release', ['dev', 'test', 'clean', 'copy', 'uglify']);
 
 };
