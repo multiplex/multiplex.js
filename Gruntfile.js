da16a27794b465c830d984ccfa229251246bf063
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-06-17 23:42:25 +0430

    grunt setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,162 @@
-﻿module.exports = function (grunt) {
+module.exports = function (grunt) {
     'use strict';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        dirs: {
+    var pkg = grunt.file.readJSON('package.json'),
+        dirs = {
             test: 'test',
             source: 'src',
             release: 'dist',
+            tasks: 'tasks',
             temp: 'dist/temp'
+        },
+        banner = [
+            '/*!',
+            '* ' + pkg.title + ' - ' + pkg.description,
+            '* Ver ' + pkg.version + ' (' + grunt.template.today('mmmm dd, yyyy') + ')',
+            '',
+            '* Created and maintained by Kamyar Nazeri <Kamyar.Nazeri@yahoo.com>',
+            '* Licensed under Apache License Version 2.0',
+            '* ' + pkg.homepage,
+            '*/',
+            ''
+        ].join('\n');
+
+
+    grunt.initConfig({
+        pkg: pkg,
+        dirs: dirs,
+        banner: banner,
+        clean: {
+            build: {
+                src: [dirs.release + '/*.*']
+            },
+            options: {
+                force: true
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        cwd: dirs.source,
+                        dest: dirs.release,
+                        src: ['**/*.{intellisense.js,d.ts}'],
+                        filter: 'isFile'
+                    }
+                ],
+                options: {
+                    process: function (content) {
+                        return banner + '\n' + content;
+                    }
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: [
+                    'Gruntfile.js',
+                    dirs.source + '/**/*.js',
+                    dirs.tasks + '/**/*.js',
+                    dirs.test + '/**/*.js'
+                ],
+                tasks: ['lint'],
+                options: {
+                    spawn: false,
+                    interrupt: true
+                }
+            }
+        },
+        jshint: {
+            files: [
+                'Gruntfile.js',
+                dirs.source + '/**/*.js',
+                dirs.tasks + '/**/*.js',
+                dirs.test + '/**/*.js'
+            ],
+            options: {
+                jshintrc: true
+            }
+        },
+        jscs: {
+            all: [
+                'Gruntfile.js',
+                dirs.source + '/**/*.js',
+                dirs.tasks + '/**/*.js',
+                dirs.test + '/**/*.js'
+            ],
+            options: {
+                config: '.jscs.json'
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: true,
+                screwIE8: true,
+                preserveComments: false,
+                report: 'min',
+                sourceMapName: dirs.release + '/multiplex.min.js.map',
+                banner: banner,
+                mangle: {
+                    sort: true,
+                    eval: true,
+                    except: [
+                        'mx',
+                        'multiplex',
+                        'Enumerable',
+                        'Enumerator',
+                        'Collection',
+                        'ReadOnlyCollection',
+                        'List',
+                        'SortedList',
+                        'Dictionary',
+                        'KeyValuePair',
+                        'HashSet',
+                        'HashTable',
+                        'LinkedList',
+                        'LinkedListNode',
+                        'Queue',
+                        'Stack',
+                        'Lookup',
+                        'Grouping',
+                        'OrderedEnumerable',
+                        'Comparer',
+                        'EqualityComparer'
+                    ]
+                }
+            },
+            dist: {
+                files: {
+                    '<%= dirs.release %>/multiplex.min.js': [dirs.release + '/multiplex.js']
+                }
+            }
+        },
+        qunit: {
+            all: [dirs.test + '/mx.html']
         }
     });
 
 
-    grunt.loadTasks('tasks');
+    grunt.loadTasks(dirs.tasks);
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('dev', []);
-    grunt.registerTask('test', []);
-    grunt.registerTask('default', []);
-    grunt.registerTask('release', []);
+
+    // linting
+    grunt.registerTask('lint', ['jshint', 'jscs']);
+
+    // test tasks
+    grunt.registerTask('test', ['transpile', 'qunit']);
+
+    // default task
+    grunt.registerTask('default', ['lint', 'test']);
+
+    // releasing a new version
+    grunt.registerTask('release', [
+        'clean',
+        'default',
+        'copy',
+        'uglify'
+    ]);
 };
--- tasks/transpile.js
+++ tasks/transpile.js
@@ -1,31 +1,35 @@
 module.exports = function (grunt) {
     var rollup = require('rollup');
+    var dirs = grunt.config.get('dirs');
+    var banner = grunt.config.get('banner');
 
     function transpileFactory(name) {
-        return function (params) {
+        return function () {
             var done = this.async();
 
             rollup.rollup({
-                entry: 'src/' + name
+                entry: dirs.source + '/' + name
             }).then(function (bundle) {
                 bundle.write({
+                    // output format - 'amd', 'cjs', 'es6', 'iife', 'umd'
                     format: 'umd',
-                    sourceMap: true,
-                    //banner: '/* banner */',
-                    //footer: '/* footer */',
-                    dest: 'dist/' + name,
+                    sourceMap: false,
+                    banner: banner,
+                    footer: ' ',
+                    dest: dirs.release + '/' + name,
                     moduleName: 'mx'
                 });
-            }).catch(function (ex) {
-                console.log('error: ' + ex);
+            }).then(done, function (e) {
+                grunt.log.error('error transpiling-custom', e);
+                done(e);
             });
         };
-    };
+    }
 
     grunt.task.registerTask('transpile-es5', 'convert es5 to umd', transpileFactory('multiplex.js'));
     grunt.task.registerTask('transpile-es6', 'convert es6 to umd', transpileFactory('multiplex-es6.js'));
 
-    grunt.task.registerTask('transpile', 'builds all files, converts es6 modules to umd', function (locales) {
+    grunt.task.registerTask('transpile', 'builds all files, converts es6 modules to umd', function () {
         var tasks = [
             'transpile-es5',
             'transpile-es6'
