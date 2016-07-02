c8cefb454a52ef07313c30b15ac6dd97b6c89953
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-03 00:50:10 +0430

    complete build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function (grunt) {
     grunt.registerTask('lint', ['jshint', 'jscs']);
 
     // test tasks
-    grunt.registerTask('test', ['transpile', 'qtest']);
+    grunt.registerTask('test', ['build', 'qtest']);
 
     // default task
     grunt.registerTask('default', ['lint', 'test']);
