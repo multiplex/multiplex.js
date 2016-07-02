a5e7f605970ac2f33b9221ec4f6136e43d537c84
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-03 00:48:39 +0430

    init hash tests

--- test/unit/core/runtime.js
+++
@@ -1,8 +0,0 @@
-import mx from '../../multiplex'
-import {qmodule, qtest} from '../../qunit'
-
-qmodule('runtime');
-
-qtest('dummy', function (assert) {
-    assert.ok(mx === mx, 'Dummy test!');
-});
