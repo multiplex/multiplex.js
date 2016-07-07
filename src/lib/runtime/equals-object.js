60b59771eff854eeec5cd0152bac0443bcccf9fa
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-07 13:18:34 +0430

    include equals-object

---
+++ src/lib/runtime/equals-object.js
@@ -0,0 +1,36 @@
+import hash from './hash';
+import equals from './equals';
+
+export default function computeObjectEquals(objA, objB) {
+    // Objects having different hash code are not equal
+    if (hash(objA) !== hash(objB)) {
+        return false;
+    }
+
+
+    /// Process equality for object literals:
+    /// object literals may have equal hash code, we process equality by each property.
+    /// regular 'class' instances have different hash code, hence do not fall into following code.
+    /// object objA is direct descendant of Object hence no need to check 'hasOwnProperty'
+
+    var _val, _prop;
+
+    for (_prop in objA) {
+        _val = objA[_prop];
+
+        /// Object methods are not considered for equality
+        if (typeof _val === 'function') {
+            continue;
+        }
+
+        if (!equals(_val, objB[_prop])) {
+            return false;
+        }
+    }
+
+    /// no need to browse objB properties, all properties of objA is checked against objB
+    /// it is very unlikely for object literals with the same hash code to have different properties
+    /// even in such a rare case, objects are considered equal
+
+    return true;
+}
