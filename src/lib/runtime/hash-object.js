672f9d872bd252992c4916d8388f583566dfddaf
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-07 11:21:59 +0430

    minor object-hash changes

--- src/lib/runtime/hash-object.js
+++ src/lib/runtime/hash-object.js
@@ -11,13 +11,12 @@ let __objetHashIndex = __objectHashSeed;
 export default function compute31BitObjecHash(obj) {
     let _hash = __objectHashMap.get(obj);
 
-    if (_hash == null) {
+    if (_hash === undefined) {
         if (isObjectLiteral(obj)) {
             _hash = __objectHashSeed;
             __objectHashMap.set(obj, 0);           // prevents recursion
 
             // only object literals fall into following code, no need to check for hasOwnProperty
-
             for (let _p in obj) {
                 _hash = combineHash(_hash, compute31BitStringHash(_p) + hash(obj[_p]));
             }
