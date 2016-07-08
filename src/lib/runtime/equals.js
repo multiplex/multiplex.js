5d9d37665fdb9c8916bae26351283fd72cc634e1
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-08 22:04:53 +0430

    update implementation

--- src/lib/runtime/equals.js
+++ src/lib/runtime/equals.js
@@ -1,91 +1,62 @@
-import hash from './hash';
-import equalsSymbol from './equals-symbol';
+import valueOf from '../utils/value-of';
+import computeObjectEquals from './equals-object';
 
 
 /**
 * Determines whether the specified object instances are considered equal.
 * @param {Object} objA The first object to compare.
 * @param {Object} objB The second object to compare.
-* @param {EqualityComparer=} comparer An equality comparer to compare values.
 * @returns {Boolean} if the objA parameter is the same instance as the objB parameter, or if both are null, or if objA.equals(objB) returns true; otherwise, false.
 */
-export default function equals(objA, objB, comparer) {
-    // Objects are identical (including null)
+export default function equals(objA, objB) {
+    // Objects are identical
     if (objA === objB) {
         return true;
     }
 
-    // null is not equal to any object
-    else if (objA == null || objB == null) {
-        return false;
-    }
 
-
-    // compare using 'equalityComparer' provided
-    if (comparer) {
-        return comparer.hash(objA) === comparer.hash(objB) && comparer.equals(objA, objB);
-    }
-
-
-    // Objects check for equality for primitive types
-    if (typeof objA === 'number' ||
-        typeof objA === 'string' ||
-        typeof objA === 'boolean') {
+    // null/undefined is not equal to any object
+    else if (
+        objA === null || objA === undefined ||
+        objB === null || objB === undefined) {
         return objA == objB;
     }
 
+
+    // built-in value types
+    else if (
+        typeof objA === 'number' ||
+        typeof objA === 'string' ||
+        typeof objA === 'boolean') {
+        return valueOf(objA) === valueOf(objB);
+    }
+
+    // object types equality
     else if (typeof objA === 'object') {
-        // Objects are from 'Date' type
-        if (objA instanceof Date) {
-            return objB instanceof Date && objA.getTime() === objB.getTime();
+        // Objects are built-in types
+        if (
+            objA instanceof Date ||
+            objA instanceof Number ||
+            objA instanceof String ||
+            objA instanceof Boolean) {
+            return valueOf(objA) === valueOf(objB);
         }
 
         // Compute overriden 'equals' method for Object types
-        else if (typeof objA[equalsSymbol] === 'function') {
-            return objA[equalsSymbol](objB);
+        else if (typeof objA.__eq__ === 'function') {
+            return objA.__eq__(objB);
         }
 
         // Object types
         else if (typeof objB === 'object') {
-            // Objects having different hash code are not equal
-            if (hash(objA) !== hash(objB)) {
-                return false;
-            }
-
-
-            /// Process equality for object literals:
-            /// object literals may have equal hash code, we process equality by each property.
-            /// regular 'class' instances have different hash code, hence do not fall into following code.
-            /// object objA is direct descendant of Object hence no need to check 'hasOwnProperty'
-
-            var _val, _prop;
-
-            for (_prop in objA) {
-                _val = objA[_prop];
-
-                /// Object methods are not considered for equality
-                if (typeof _val === 'function') {
-                    continue;
-                }
-
-                if (!equals(_val, objB[_prop])) {
-                    return false;
-                }
-            }
-
-            /// no need to browse objB properties, all properties of objA is checked against objB
-            /// it is very unlikely for object literals with the same hash code to have different properties
-            /// even in such a rare case, objects are considered equal
-
-            return true;
+            return computeObjectEquals(objA, objB);
         }
 
-        // Objects are equal (with auto type conversion)
-        // Objects from the same type are considered equal (eg. new Number(1) and 1)
-        return objA == objB;
+        // Objects are already not equal
+        return false;
     }
 
 
-    // Objects are already not equal
-    return false;
+    // Other types: check with auto type conversion
+    return objA == objB;
 }
