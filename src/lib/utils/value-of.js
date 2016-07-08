80462f20cfde3dccc1bf8423b7ec65b497fcc2a6
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-08 22:00:24 +0430

    include Date value

--- src/lib/utils/value-of.js
+++ src/lib/utils/value-of.js
@@ -1,3 +1,11 @@
-export default function value(obj) {
-    return typeof obj.valueOf === 'function' ? obj.valueOf() : 0;
+export default function valueOf(obj) {
+    if (obj === null || obj === undefined) {
+        return 0;
+    }
+    else if (obj instanceof Date) {
+        return typeof obj.getTime === 'function' ? obj.getTime() : 0;
+    }
+    else {
+        return typeof obj.valueOf === 'function' ? obj.valueOf() : 0;
+    }
 }
