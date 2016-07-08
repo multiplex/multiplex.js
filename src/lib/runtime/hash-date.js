b6cacbcc6ca53de46f64eee9b0ea97c75013e98d
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-08 22:02:31 +0430

    remove date.valueOf in favor of valueOf function

--- src/lib/runtime/hash-date.js
+++ src/lib/runtime/hash-date.js
@@ -1,4 +1,6 @@
+import valueOf from '../utils/value-of';
+
 export default function compute31BitDateHash(val) {
-    let _time = val.getTime();
+    let _time = valueOf(val);
     return _time ^ (_time >> 5);
 }
