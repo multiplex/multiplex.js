3337cb2f04a0ccc7879ee22a9193d09e96d87325
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-07-08 23:47:46 +0430

    fix string compare using localeCompare

--- build/multiplex.js
+++ build/multiplex.js
@@ -490,7 +490,8 @@
 
         // Strings are compared using String.prototype.localeCompare method
         else if (typeof objA === 'string') {
-            return objA.localeCompare(objB);
+            let _res = objA.localeCompare(objB);
+            return _res > 0 ? 1 : (_res < 0 ? -1 : 0);
         }
 
         // Compute overriden 'compare' method for Object types
