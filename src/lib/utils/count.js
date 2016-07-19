9c5dfa8ea32d68ae83cfa9425e6e5945833467c7
kamyar nazeri <kamyar nazeri>
2016-07-19 23:21:13 +0430

    implement count function

--- src/lib/utils/count.js
+++ src/lib/utils/count.js
@@ -1,4 +1,22 @@
+import Iterable from '../iteration/iterable';
+import Collection from '../collections/collection';
+import isArrayLike from './is-array-like';
+
 // Gets number of items if the value is a Collection, Array or an Array-like, otherwise returns -1.
 export default function count(value) {
-    return value;
+    if (isArrayLike(value)) {
+        return value.length;
+    }
+
+    else if (value instanceof Collection) {
+        return value.count();
+    }
+
+    else if (value instanceof Iterable) {
+        if (isArrayLike(value.valueOf())) {
+            return value.valueOf().length;
+        }
+    }
+
+    return -1;
 }
