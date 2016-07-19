7ae0785a54ec2560e4324e4a75db45b4265b0d07
kamyar nazeri <kamyar nazeri>
2016-07-19 22:48:38 +0430

    include constant error messages

--- src/lib/utils/error.js
+++ src/lib/utils/error.js
@@ -2,6 +2,5 @@ export default function error(msg) {
     throw new Error(msg);
 }
 
-export function arraySizeError() {
-    error('The number of elements in the source is greater than the number of elements that the destination array can contain.');
-}
+export var ERROR_ARRAY_SIZE = 'The number of elements in the source is greater than the number of elements that the destination array can contain.';
+export var ERROR_NOT_IMPLEMENTED = 'Method not implemented.';
