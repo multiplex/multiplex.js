4cc3040188a66b11923e1b22a2208ee3021e6253
kamyar nazeri <kamyar nazeri>
2016-07-29 15:20:44 +0430

    assertNotNull

--- src/lib/linq/select.js
+++ src/lib/linq/select.js
@@ -1,7 +1,9 @@
 import Iterable from '../iteration/iterable';
 import assertType from '../utils/assert-type';
+import assertNotNull from '../utils/assert-not-null';
 
 export default function selectIterator(source, selector) {
+    assertNotNull(source);
     assertType(selector, Function);
 
     return new Iterable(function* () {
--- src/lib/linq/where.js
+++ src/lib/linq/where.js
@@ -1,7 +1,9 @@
 import Iterable from '../iteration/iterable';
 import assertType from '../utils/assert-type';
+import assertNotNull from '../utils/assert-not-null';
 
 export default function whereIterator(source, predicate) {
+    assertNotNull(source);
     assertType(predicate, Function);
 
     return new Iterable(function* () {
