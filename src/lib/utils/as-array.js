1dc37b5e77d3d1d3448a024544baf7dc1a9e1296
kamyar nazeri <kamyar nazeri>
2016-07-30 01:33:00 +0430

    include asArray method

---
+++ src/lib/utils/as-array.js
@@ -0,0 +1,14 @@
+import isArrayLike from './is-array-like';
+import ArrayIterable from '../iteration/iterable-array';
+
+export default function asArray(value) {
+    if (isArrayLike(value)) {                       // array-likes have fixed element count
+        return value;
+    }
+
+    else if (value instanceof ArrayIterable) {      // ArrayIterable wrapper
+        return value.valueOf();
+    }
+
+    return null;
+}
