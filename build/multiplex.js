2758e3329f0ddee775da00202c4a4b0a906ef008
kamyar nazeri <kamyar nazeri>
2016-07-30 21:13:10 +0430

    include contains linq method

--- build/multiplex.js
+++ build/multiplex.js
@@ -1132,6 +1132,89 @@
         });
     }
 
+    /**
+    * Provides a base class for implementations of the EqualityComparer.
+    */
+    function EqualityComparer(hashCodeProvider, equality) {
+        assertType(hashCodeProvider, Function);
+        assertType(equality, Function);
+
+        this._hash = hashCodeProvider;
+        this._equals = equality;
+    }
+
+
+    var defaultEqualityComparer = new EqualityComparer(hash, equals);
+
+
+    mixin(EqualityComparer.prototype, {
+        /**
+        * Determines whether the specified objects are equal.
+        * @param {Object} x The first object of type Object to compare.
+        * @param {Object} y The second object of type Object to compare.
+        * @returns true if the specified objects are equal; otherwise, false.
+        */
+        equals: function (x, y) {
+            return this._equals(x, y);
+        },
+
+        /**
+        * Returns a hash code for the specified object.
+        * @param {Object} obj The Object for which a hash code is to be returned.
+        * @returns A hash code for the specified object.
+        */
+        hash: function (obj) {
+            return this._hash(obj);
+        },
+
+        toString: function () {
+            return '[EqualityComparer]';
+        }
+    });
+
+
+    mixin(EqualityComparer, {
+        /**
+        * Gets a default sort order comparer for the type specified by the generic argument.
+        */
+        defaultComparer: defaultEqualityComparer,
+
+        /**
+        * Gets or creates a new EqualityComparer object.
+        * @param {EqualityComparer|Object} value An EqualityComparer object.
+        * @returns {EqualityComparer}
+        */
+        from: function (value) {
+            if (value instanceof EqualityComparer) {
+                return value;
+            }
+
+            else if (value && isFunction(value.hash) && isFunction(value.equals)) {
+                return new EqualityComparer(value.hash, value.equals);
+            }
+
+            else {
+                return defaultEqualityComparer;
+            }
+        }
+    });
+
+    function containsIterator(source, value, comparer) {
+        assertNotNull(source);
+        comparer = EqualityComparer.from(comparer);
+
+        var result = false;
+
+        forOf(source, function (element) {
+            if (comparer.equals(element, value)) {
+                result = true;
+                return result;
+            }
+        });
+
+        return result;
+    }
+
     function whereIterator(source, predicate) {
         assertNotNull(source);
         assertType(predicate, Function);
@@ -1368,6 +1451,16 @@
             */
             concat: function (second) {
                 return concatIterator(this, second);
+            },
+
+            /**
+            * Determines whether a sequence contains a specified element by using an equality comparer.
+            * @param {Object} value The value to locate in the sequence.
+            * @param {EqualityComparer=} comparer An equality comparer to compare values.
+            * @returns {Boolean}
+            */
+            contains: function (value, comparer) {
+                return containsIterator(this, value, comparer);
             },
 
             /**
