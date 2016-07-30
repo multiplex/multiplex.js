ffea2300d45bee00a92c5f7f5ab81468647f2c0d
kamyar nazeri <kamyar nazeri>
2016-07-30 21:54:37 +0430

    draft HashSet

--- src/lib/collections/hash-set.js
+++ src/lib/collections/hash-set.js
@@ -1,3 +1,8 @@
+import Collection from './collection';
+import extend from '../utils/extend';
+
 export default function HashSet(comparer) {
     this._comparer = comparer;
 }
+
+extend(HashSet, Collection);
