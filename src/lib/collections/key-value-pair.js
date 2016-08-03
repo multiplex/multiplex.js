658226384cf3786e1c33dbc1b9fec5919db904f3
kamyar nazeri <kamyar nazeri>
2016-08-04 01:51:00 +0430

    include key-value-pair class

---
+++ src/lib/collections/key-value-pair.js
@@ -0,0 +1,13 @@
+/**
+* Initializes a new instance of the KeyValuePair with the specified key and value.
+* @param {Object} key The object defined in each key/value pair.
+* @param {Object} value The definition associated with key.
+*/
+export default class KeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+
+        Object.freeze(this);
+    }
+}
