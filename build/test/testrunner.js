7da8728c6a0c5c110c70bb231d76da7e70cd304d
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-05 02:50:15 +0430

    buid with new testrunner

--- build/test/testrunner.js
+++ build/test/testrunner.js
@@ -1,9 +1,20 @@
-require([
-    'collections/collection.js',
-    'core/create.js',
-    'core/iterable.js',
-    'core/iterator.js',
-    'runtime/compare.js',
-    'runtime/equals.js',
-    'runtime/hash.js'
-]);
+var modules = [
+    './collections/collection.js',
+    './core/create.js',
+    './core/iterable.js',
+    './core/iterator.js',
+    './runtime/compare.js',
+    './runtime/equals.js',
+    './runtime/hash.js'
+];
+
+(function () {
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+        for (var i = 0; i < modules.length; i++) {
+            require(modules[i]);
+        }
+    }
+    else if (typeof define === 'function') {
+        require(modules);
+    }
+})();
