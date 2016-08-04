da37e90a5c3bc04680c1079a42d5996e9ec22e30
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-05 03:26:21 +0430

    allow mixin properties to be writable by default

--- src/lib/utils/mixin.js
+++ src/lib/utils/mixin.js
@@ -5,7 +5,7 @@ export default function mixin(obj, properties, attributes) {
         if (properties.hasOwnProperty(_prop)) {
             Object.defineProperty(obj, _prop, {
                 value: properties[_prop],
-                writable: attributes.writable || false,
+                writable: attributes.writable || true,
                 enumerable: attributes.enumerable || false,
                 configurable: attributes.configurable || false
             });
