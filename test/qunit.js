785f259c3749071b624856e57810afb1d91481c3
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-05 02:47:09 +0430

    include qunit module

--- test/qunit.js
+++ test/qunit.js
@@ -1,4 +1,5 @@
-export var qmodule = QUnit.module;
-export var qtest = QUnit.test;
-export var qexpect = QUnit.expect;
+var qunit = typeof QUnit === 'undefined' ? require('qunitjs') : QUnit;
+export var qmodule = qunit.module;
+export var qtest = qunit.test;
+export var qexpect = qunit.expect;
 
