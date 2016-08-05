0c5655a9f150ede5f14bbca9a1b670c2579ec8e5
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-05 22:28:05 +0430

    use HashTable with union

--- src/lib/linq/union.js
+++ src/lib/linq/union.js
@@ -1,24 +1,22 @@
 import Iterable from '../iteration/iterable';
 import Iterator from '../iteration/iterator';
+import HashTable from '../collections/hash-table';
 import iterator from '../iteration/iterator-factory';
 import assertNotNull from '../utils/assert-not-null';
-import HashSet from '../collections/hash-set';
-import EqualityComparer from '../collections/equality-comparer';
 
 export default function unionIterator(first, second, comparer) {
     assertNotNull(first);
     assertNotNull(second);
-    comparer = EqualityComparer.from(comparer);
 
     return new Iterable(function () {
-        var set = new HashSet(comparer),
+        var table = new HashTable(0, comparer),
             it1 = iterator(first),
             it2 = iterator(second),
             next;
 
         return new Iterator(function () {
             while (!(next = it1.next()).done || !(next = it2.next()).done) {
-                if (set.add(next.value)) {
+                if (table.add(next.value)) {
                     return {
                         value: next.value,
                         done: false
