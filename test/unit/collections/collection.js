51de66ad741c2a2699150999530328c91cfbacea
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-07 22:50:42 +0430

    update collection tests

--- test/unit/collections/collection.js
+++ test/unit/collections/collection.js
@@ -24,24 +24,6 @@ qtest('create collection', function (assert) {
 });
 
 
-qtest('collection toArray', function (assert) {
-    assert.deepEqual(new Collection().toArray(), [], 'empty collection toArray');
-    assert.deepEqual(new Collection([1, 2, 3]).toArray(), [1, 2, 3], 'create collection from array then toarray');
-    assert.deepEqual(new Collection('string').toArray(), ['s', 't', 'r', 'i', 'n', 'g'], 'create collection from string and toarray');
-    assert.deepEqual(new Collection(arguments).toArray(), [assert], 'create collection from arguments and toArray');
-    assert.deepEqual(new Collection(new Collection([1])).toArray(), [1], 'create collection from another collection and toArray');
-    assert.deepEqual(new Collection(mx([1, 2, 3])).toArray(), [1, 2, 3], 'create collection from ArrayIterable and toArray');
-    assert.deepEqual(new Collection(mx({ val: 1 })).toArray(), [['val', 1]], 'create collection from ObjectIterable and toArray');
-    assert.deepEqual(new Collection(mx.repeat(0, 1000)).toArray(), mx.repeat(0, 1000).toArray(), 'create collection from big array');
-    assert.deepEqual(new Collection(mx([1, 2, 3]).select(t => t * 2)).toArray(), [2, 4, 6], 'create collection from Iterable and toArray');
-    assert.deepEqual(new Collection(function* () {
-        yield 1;
-        yield 2;
-        yield 3;
-    }).toArray(), [1, 2, 3], 'create collection from Generator and toArray');
-});
-
-
 qtest('collection count', function (assert) {
     assert.equal(new Collection().count(), 0, 'empty collection');
     assert.equal(new Collection([1, 2, 3]).count(), 3, 'create collection from array');
