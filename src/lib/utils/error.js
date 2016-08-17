2fcb1a45fab34326817f974255add5a225c89e7e
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-17 22:34:29 +0430

    update error list

--- src/lib/utils/error.js
+++ src/lib/utils/error.js
@@ -10,3 +10,4 @@ export var ERROR_NO_MATCH = 'Sequence contains no matching element.';
 export var ERROR_NON_NUMERIC_TYPE = 'Value is not a number.';
 export var ERROR_MORE_THAN_ONE_ELEMENT = 'Sequence contains more than one element.';
 export var ERROR_KEY_NOT_FOUND = 'The given key was not present in the collection.';
+export var ERROR_DUPLICATE_KEY = 'An item with the same key has already been added.';
