0241d3435fd6c44728b0bd28a9bd5bdbe54d34e8
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-21 00:25:06 +0430

    complete ReadOnlyCollection

--- src/lib/collections/read-only-collection.js
+++ src/lib/collections/read-only-collection.js
@@ -1,7 +1,20 @@
 import Collection from './collection';
+import ArrayIterator from '../iteration/iterator-array';
 
 export default class ReadOnlyCollection extends Collection {
     constructor(list) {
         super(list);
+    }
+
+    get [Symbol.toStringTag]() {
+        return 'ReadOnly Collection';
+    }
+
+    toString() {
+        return '[ReadOnly Collection]';
+    }
+
+    [Symbol.iterator]() {
+        return new ArrayIterator(this);
     }
 }
