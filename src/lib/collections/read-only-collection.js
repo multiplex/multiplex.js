e2cff0b308f2913ede88fb33ccf81da2a95a9e84
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-27 23:18:22 +0430

    initialize ReadOnlyCollection

--- src/lib/collections/read-only-collection.js
+++ src/lib/collections/read-only-collection.js
@@ -1,4 +1,6 @@
 import Collection from './collection';
+import ArrayIterator from '../iteration/iterator-array';
+import {ARRAY_PROTOTYPE} from '../utils/builtin-types';
 import extend from '../utils/extend';
 
 export default function ReadOnlyCollection(list) {
@@ -6,4 +8,23 @@ export default function ReadOnlyCollection(list) {
 }
 
 extend(ReadOnlyCollection, Collection, {
+    /**
+    * Returns a shallow copy of a portion of the list into a new array object.
+    * @param {Number=} begin Zero-based index at which to begin extraction.
+    * @param {Number=} end Zero-based index at which to end extraction
+    * @returns {Array}
+    */
+    slice: ARRAY_PROTOTYPE.slice,
+
+    valueOf: function () {
+        return this.slice();
+    },
+
+    toString: function () {
+        return '[ReadOnly Collection]';
+    },
+
+    '@@iterator': function () {
+        return new ArrayIterator(this);
+    }
 });
