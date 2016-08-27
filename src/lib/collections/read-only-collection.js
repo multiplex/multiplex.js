8942ab61cf61407757cdc69cd6b607e0049c1047
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-08-27 23:18:26 +0430

    initialize ReadOnlyCollection

--- src/lib/collections/read-only-collection.js
+++ src/lib/collections/read-only-collection.js
@@ -1,9 +1,24 @@
 import Collection from './collection';
 import ArrayIterator from '../iteration/iterator-array';
+import {ARRAY_PROTOTYPE} from '../utils/builtin-types';
 
 export default class ReadOnlyCollection extends Collection {
     constructor(list) {
         super(list);
+    }
+
+    /**
+    * Returns a shallow copy of a portion of the list into a new array object.
+    * @param {Number=} begin Zero-based index at which to begin extraction.
+    * @param {Number=} end Zero-based index at which to end extraction
+    * @returns {Array}
+    */
+    slice(begin = 0, end = undefined) {
+        return ARRAY_PROTOTYPE.slice.call(this, begin, end === undefined ? this.length : end);
+    }
+
+    valueOf() {
+        return this.slice();
     }
 
     get [Symbol.toStringTag]() {
