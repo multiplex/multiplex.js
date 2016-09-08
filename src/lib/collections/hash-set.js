8c36f9ff714317e9425f4f8c199538ef26dfcdf2
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-09-08 23:23:36 +0430

    update HashSet class

--- src/lib/collections/hash-set.js
+++ src/lib/collections/hash-set.js
@@ -3,6 +3,14 @@ import Collection from './collection';
 export default class HashSet extends Collection {
     constructor(comparer) {
         super();
-        this._comparer = comparer;
+        this.comparer = comparer;
+    }
+
+    get [Symbol.toStringTag]() {
+        return 'HashSet';
+    }
+
+    toString() {
+        return '[HashSet]';
     }
 }
