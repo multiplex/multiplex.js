f199bd0d8f406651c56e806109574680cfddd18f
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-09-16 18:49:05 +0430

    initialize HashSet

--- src/lib/collections/hash-set.js
+++ src/lib/collections/hash-set.js
@@ -1,9 +1,18 @@
 import Collection from './collection';
+import HashTable from './hash-table';
+import EqulityComparer from './equality-comparer';
+import iterable from '../iteration/iterable-factory';
 
 export default class HashSet extends Collection {
-    constructor(comparer) {
+    constructor(source = null, comparer = EqulityComparer.instance) {
         super();
-        this.comparer = comparer;
+        this.table = new HashTable(EqulityComparer.from(comparer));
+
+        if (source) {
+            for (let element of iterable(source)) {
+                this.table.add(element);
+            }
+        }
     }
 
     get [Symbol.toStringTag]() {
