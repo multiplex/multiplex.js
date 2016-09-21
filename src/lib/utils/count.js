897787e49dd934a4eb3d187625b9e1504a94286e
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-09-21 21:52:38 +0330

    apply collectionOnly parameter to the count method

--- src/lib/utils/count.js
+++ src/lib/utils/count.js
@@ -3,7 +3,13 @@ import ArrayIterable from '../iteration/iterable-array';
 import isArrayLike from './is-array-like';
 import $iterator from '../iteration/iterator-factory';
 
-export default function count(value) {
+/**
+* Gets number of items in the specified iterable object.
+* @param {Iterable} value An Iterable object.
+* @param {Boolean} collectionOnly when true returns the number of items in iterable if the value is a Collection, Array or an Array-like, otherwise returns -1.
+* @returns {Number}
+*/
+export default function count(value, collectionOnly) {
     if (isArrayLike(value)) {
         return value.length;
     }
@@ -16,7 +22,7 @@ export default function count(value) {
         return value.count();
     }
 
-    else {
+    else if (!collectionOnly) {
         var it = $iterator(value),
             count = 0;
 
@@ -26,4 +32,6 @@ export default function count(value) {
 
         return count;
     }
+
+    return -1;
 }
