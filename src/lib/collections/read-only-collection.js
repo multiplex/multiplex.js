92d365e9bfe4715d3ac33d090f06b7b43b2fc2d7
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-09-25 23:12:15 +0330

    use array-like in ReadOnlyCollection

--- src/lib/collections/read-only-collection.js
+++ src/lib/collections/read-only-collection.js
@@ -1,12 +1,17 @@
-import List from './list';
 import Collection from './collection';
 import ArrayIterator from '../iteration/iterator-array';
-import {ARRAY_PROTOTYPE} from '../utils/builtin-types';
-import assertType from '../utils/assert-type';
+import isArrayLike from '../utils/is-array-like';
+import error from '../utils/error';
+import {
+    ARRAY_PROTOTYPE
+} from '../utils/builtin-types';
 
 export default class ReadOnlyCollection extends Collection {
     constructor(list) {
-        assertType(list, List);
+        if (!isArrayLike(list)) {
+            error('Invalid argument!');
+        }
+
         super(list);
         this.list = list;
 
@@ -18,78 +23,78 @@ export default class ReadOnlyCollection extends Collection {
     }
 
     /**
-    * Gets the number of elements contained in the ReadOnlyCollection.
-    * @returns {Number}
-    */
+     * Gets the number of elements contained in the ReadOnlyCollection.
+     * @returns {Number}
+     */
     count() {
         return this.length;
     }
 
     /**
-    * Determines whether the ReadOnlyCollection contains a specific value.
-    * @param {Object} item The object to locate in the ReadOnlyCollection.
-    * @returns {Boolean}
-    */
+     * Determines whether the ReadOnlyCollection contains a specific value.
+     * @param {Object} item The object to locate in the ReadOnlyCollection.
+     * @returns {Boolean}
+     */
     contains(item) {
         this.list.contains(item);
     }
 
     /**
-    * Gets the element at the specified index.
-    * @param {Number} index The zero-based index of the element to get.
-    * @returns {Object}
-    */
+     * Gets the element at the specified index.
+     * @param {Number} index The zero-based index of the element to get.
+     * @returns {Object}
+     */
     get(index) {
         return this.list.get(index);
     }
 
     /**
-    * Gets the number of elements contained in the ReadOnlyCollection.
-    * @returns {Number}
-    */
+     * Gets the number of elements contained in the ReadOnlyCollection.
+     * @returns {Number}
+     */
     get length() {
         return this.list.length;
     }
 
     /**
-    * Searches for the specified object and returns the zero-based index of the first occurrence within the entire ReadOnlyCollection.
-    * @param {Object} item The object to locate in the ReadOnlyCollection.
-    * @returns {Number}
-    */
+     * Searches for the specified object and returns the zero-based index of the first occurrence within the entire ReadOnlyCollection.
+     * @param {Object} item The object to locate in the ReadOnlyCollection.
+     * @returns {Number}
+     */
     indexOf(item) {
         return this.list.indexOf(item);
     }
 
     /**
-    * Returns a shallow copy of a portion of the list into a new array object.
-    * @param {Number=} begin Zero-based index at which to begin extraction.
-    * @param {Number=} end Zero-based index at which to end extraction
-    * @returns {Array}
-    */
+     * Returns a shallow copy of a portion of the list into a new array object.
+     * @param {Number=} begin Zero-based index at which to begin extraction.
+     * @param {Number=} end Zero-based index at which to end extraction
+     * @returns {Array}
+     */
     slice(begin = 0, end = undefined) {
         return ARRAY_PROTOTYPE.slice.call(this, begin, end === undefined ? this.length : end);
     }
 
     /**
-    * Changes the content of the list by removing existing elements and/or adding new elements.
-    * @param {Number} start Index at which to start changing the list.
-    * @param {Number} deleteCount An integer indicating the number of old list elements to remove.
-    * @param {Object...} items The elements to add to the list.
-    * @returns {Array}
-    */
+     * Changes the content of the list by removing existing elements and/or adding new elements.
+     * @param {Number} start Index at which to start changing the list.
+     * @param {Number} deleteCount An integer indicating the number of old list elements to remove.
+     * @param {Object...} items The elements to add to the list.
+     * @returns {Array}
+     */
     splice(start, deleteCount, ...items) {
         return ARRAY_PROTOTYPE.splice.call(this, start, deleteCount, items);
     }
 
     /**
-    * Buffers collection into an array.
-    * @returns {Array}
-    */
+     * Buffers collection into an array.
+     * @returns {Array}
+     */
     toArray() {
         return this.list.toArray();
     }
 
-    get [Symbol.toStringTag]() {
+    get[Symbol.toStringTag]() {
         return 'ReadOnly Collection';
     }
 
