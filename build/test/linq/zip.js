df104ecc4f874838e8b0dd5653c23a62ce118322
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-09-29 13:41:39 +0330

    linq zip tests

--- build/test/linq/zip.js
+++ build/test/linq/zip.js
@@ -12,8 +12,40 @@ var qtest = qunit.test;
 
 qmodule('linq-zip');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+qtest('homogeneous zip', function (assert) {
+    assert.deepEqual(mx([1, 2]).zip([3, 4], function (t, u) {
+        return t + u;
+    }).toArray(), [4, 6], 'Zip two numeric array!');
+});
+
+
+qtest('heterogeneous zip', function (assert) {
+    assert.deepEqual(mx([1, 2]).zip([3], function (t, u) {
+        return t + u;
+    }).toArray(), [4], 'Zip two numeric array!');
+
+    assert.deepEqual(mx([]).zip([3], function (t, u) {
+        return t + u;
+    }).toArray(), [], 'Zip an empty iterable with anything results in an empty iterable!');
+
+    assert.deepEqual(mx([1, 2]).zip([], function (t, u) {
+        return t + u;
+    }).toArray(), [], 'Zip anything with an empty iterable results in an empty iterable!');
+});
+
+
+qtest('zip method validations', function (assert) {
+    assert.throws(function () {
+        mx([1]).zip();
+    }, 'null input');
+
+    assert.throws(function () {
+        mx([1]).zip([2]);
+    }, 'null result selector');
+
+    assert.throws(function () {
+        mx([1]).zip([2], 1);
+    }, 'non-function result selector');
 });
 
 })));
