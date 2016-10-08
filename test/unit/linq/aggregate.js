18a377d84f416fe032490e8e83ada2b77019609e
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-10-08 22:04:35 +0330

    update linq aggregate tests

--- test/unit/linq/aggregate.js
+++ test/unit/linq/aggregate.js
@@ -45,6 +45,18 @@ qtest('collections aggregate', function (assert) {
 
 
 
+    assert.equal(mx(mocks.readOnlyCollection).aggregate(10, function (a, b) {
+        return a + b;
+    }), 25, 'Aggregate readOnlyCollection of numbers with seed!');
+
+    assert.equal(mx(mocks.readOnlyCollection).aggregate(10, function (a, b) {
+        return a + b;
+    }, function (t) {
+        return t * 2;
+    }), 50, 'Aggregate readOnlyCollection of numbers with seed and result selector!');
+
+
+
     assert.equal(mx(mocks.linkedList).aggregate(10, function (a, b) {
         return a + b;
     }), 25, 'Aggregate linkedList of numbers with seed!');
@@ -138,18 +150,6 @@ qtest('collections aggregate', function (assert) {
     }, function (t) {
         return t * 2;
     }), 50, 'Aggregate sortedList of numbers with seed and result selector!');
-
-
-
-    assert.equal(mx(mocks.readOnlyCollection).aggregate(10, function (a, b) {
-        return a + b;
-    }), 25, 'Aggregate readOnlyCollection of numbers with seed!');
-
-    assert.equal(mx(mocks.readOnlyCollection).aggregate(10, function (a, b) {
-        return a + b;
-    }, function (t) {
-        return t * 2;
-    }), 50, 'Aggregate readOnlyCollection of numbers with seed and result selector!');
 
 
 
