e88c41c64d628dc13198a3fa5053d252733dde51
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-10-14 22:24:47 +0330

    Complete Comparer tests

--- test/unit/collections/comparer.js
+++ test/unit/collections/comparer.js
@@ -31,6 +31,29 @@ qtest('basic comparer tests', function (assert) {
     assert.equal(com.compare(0, 1), -1, 'simple numeric less-than compare');
 });
 
+qtest('comparer factory method', function (assert) {
+    var com = new Comparer(numericComparerFunction);
+    assert.equal(Comparer.from(), Comparer.instance, 'create from undefined value');
+    assert.equal(Comparer.from(null), Comparer.instance, 'create from null value');
+    assert.equal(Comparer.from(1), Comparer.instance, 'create from invalid value');
+    assert.equal(Comparer.from(com), com, 'create from Comparer instance');
+    assert.equal(Comparer.from(numericComparerFunction).compare, numericComparerFunction, 'create from factory function');
+    assert.equal(Comparer.from({
+        compare: numericComparerFunction
+    }).compare, numericComparerFunction, 'create from object literal');
+});
+
+
+qtest('comparer validations', function (assert) {
+    assert.throws(function () {
+        return new Comparer();
+    }, 'null/undefined comparison');
+
+    assert.throws(function () {
+        return new Comparer(1);
+    }, 'non-function comparison');
+});
+
 qtest('comparer toString', function (assert) {
     assert.equal(new Comparer(numericComparerFunction).toString(), '[Comparer]', 'Comparer toString');
 });
