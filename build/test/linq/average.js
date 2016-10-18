92ff7d57aeea63e5a7a991c4b661d722a0336860
Kamyar Nazeri <kamyar.nazeri@yahoo.com>
2016-10-18 18:34:50 +0330

    linq average tests

--- build/test/linq/average.js
+++ build/test/linq/average.js
@@ -6,14 +6,37 @@
 
 mx = 'default' in mx ? mx['default'] : mx;
 
+var array = [1, 2, 3, 4, 5];
+var collection = new mx.Collection(array);
+var list = new mx.List(array);
+var linkedList = new mx.LinkedList(array);
+var hashSet = new mx.HashSet(array);
+var stack = new mx.Stack(array);
+var queue = new mx.Queue(array);
+var set = new mx.Set(array);
+var map = new mx.Map();
+var dictionary = new mx.Dictionary();
+var sortedList = new mx.SortedList();
+var readOnlyCollection = list.asReadOnly();
+var lookup = new mx.Lookup(array, function (t) {
+    return t;
+});
+
+for (var i = 0; i < array.length; i++) {
+    map.set(array[i], array[i]);
+    dictionary.set(array[i], array[i]);
+    sortedList.add(array[i], array[i]);
+}
+
 var qunit = typeof QUnit === 'undefined' ? require('qunitjs') : QUnit;
 var qmodule = qunit.module;
 var qtest = qunit.test;
 
 qmodule('linq-average');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+
+qtest('basic average test', function (assert) {
+    assert.equal(mx(array).average(), 3, 'Test average of first 5 numbers');
 });
 
 })));
--- test/unit/linq/average.js
+++ test/unit/linq/average.js
@@ -1,8 +1,17 @@
 import mx from '../../multiplex';
-import {qmodule, qtest} from '../../qunit';
+import * as mocks from './_mocks';
+import {
+    qmodule,
+    qtest
+} from '../../qunit';
 
 qmodule('linq-average');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+
+function simpleNumericSelector(t) {
+    return t * 2;
+}
+
+qtest('basic average test', function (assert) {
+    assert.equal(mx(mocks.array).average(), 3, 'Test average of first 5 numbers');
 });
