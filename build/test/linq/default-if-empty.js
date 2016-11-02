ce3ba050d268cbc57174eef4f6b2391823d4a2b4
kamyar nazeri <kamyar.nazeri@yahoo.com>
2016-11-02 22:59:45 +0330

    linq 'defaultIfEmpty' tests

--- build/test/linq/default-if-empty.js
+++ build/test/linq/default-if-empty.js
@@ -6,14 +6,37 @@
 
 mx = 'default' in mx ? mx['default'] : mx;
 
+var array = [1, 2, 3, 4, 5];
+var collection = new mx.Collection(array);
+var list = new mx.List(array);
+var linkedList = new mx.LinkedList(array);
+var hashSet = new mx.HashSet(array);
+var stack = new mx.Stack(array);
+var queue = new mx.Queue(array);
+var set = new mx.Set(array);
+var map = new mx.Map();
+var dictionary = new mx.Dictionary();
+var sortedList = new mx.SortedList();
+var readOnlyCollection = list.asReadOnly();
+var lookup = new mx.Lookup(array, function (t) {
+    return t;
+});
+
+for (var i = 0; i < array.length; i++) {
+    map.set(array[i], array[i]);
+    dictionary.set(array[i], array[i]);
+    sortedList.add(array[i], array[i]);
+}
+
 var qunit = typeof QUnit === 'undefined' ? require('qunitjs') : QUnit;
 var qmodule = qunit.module;
 var qtest = qunit.test;
 
 qmodule('linq-default-if-empty');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+qtest('basic "defaultIfEmpty" test', function (assert) {
+    assert.equal(mx(array).defaultIfEmpty().count(), 5, 'defaultIfEmpty on a non-empty array');
+    assert.equal(mx([]).defaultIfEmpty(1).toArray()[0], 1, 'defaultIfEmpty on an empty array');
 });
 
 })));
--- test/unit/linq/default-if-empty.js
+++ test/unit/linq/default-if-empty.js
@@ -1,8 +1,13 @@
 import mx from '../../multiplex';
-import {qmodule, qtest} from '../../qunit';
+import * as mocks from './_mocks';
+import {
+    qmodule,
+    qtest
+} from '../../qunit';
 
 qmodule('linq-default-if-empty');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+qtest('basic "defaultIfEmpty" test', function (assert) {
+    assert.equal(mx(mocks.array).defaultIfEmpty().count(), 5, 'defaultIfEmpty on a non-empty array');
+    assert.equal(mx([]).defaultIfEmpty(1).toArray()[0], 1, 'defaultIfEmpty on an empty array');
 });
