9222ebb886d49a5542d73ce252b6f900a21b5936
Kamyar Nazeri <kamyar.nzr@gmail.com>
2017-05-19 20:40:46 -0400

    .

--- build/multiplex.js
+++ build/multiplex.js
@@ -1,6 +1,6 @@
 /*!
 * Multiplex.js - Comprehensive data-structure and LINQ library for JavaScript.
-* Version 2.0.0 (May 18, 2017)
+* Version 2.0.0 (May 19, 2017)
 
 * Created and maintained by Kamyar Nazeri <Kamyar.Nazeri@yahoo.com>
 * Licensed under MIT License
