3247e3b1260e870af35eab3f2dcf2284de633345
kamyarnazeri <kamyar.nzr@gmail.com>
2017-07-26 19:47:32 -0400

    linq 'union' unit tests

--- build/test/linq/union.js
+++ build/test/linq/union.js
@@ -6,14 +6,87 @@
 
 mx = 'default' in mx ? mx['default'] : mx;
 
+var array = [1, 2, 3, 4, 5];
+var collection = new mx.Collection(array);
+var list = new mx.List(array);
+var linkedList = new mx.LinkedList(array);
+var hashSet = new mx.HashSet(array);
+var stack = new mx.Stack(array);
+var queue = new mx.Queue(array);
+var set = new mx.Set(array);
+var map = new mx.Map();
+var dictionary = new mx.Dictionary();
+var sortedList = new mx.SortedList();
+var readOnlyCollection = list.asReadOnly();
+var lookup = new mx.Lookup(array, function (t) {
+    return t;
+});
+
+for (var i = 0; i < array.length; i++) {
+    map.set(array[i], array[i]);
+    dictionary.set(array[i], array[i]);
+    sortedList.add(array[i], array[i]);
+}
+
 var qunit = typeof QUnit === 'undefined' ? require('qunitjs') : QUnit;
 var qmodule = qunit.module;
 var qtest = qunit.test;
 
 qmodule('linq-union');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+
+qtest('basic "union" test', function (assert) {
+    assert.deepEqual(mx([]).union([]).toArray(), [], 'union two empty arrays');
+    assert.deepEqual(mx([1, 2]).union([]).toArray(), [1, 2], 'union an array with empty array');
+    assert.deepEqual(mx([1, 2]).union([3, 4]).toArray(), [1, 2, 3, 4], 'union two arrays');
+    assert.deepEqual(mx([1, 2]).union([1, 2]).toArray(), [1, 2], 'union two identical arrays');
+});
+
+
+qtest('collections "union" method tests', function (assert) {
+    var len = array.length;
+    assert.equal(collection.union([]).toArray().length, len, 'Test "union" with an empty array in a Collection');
+    assert.equal(collection.union(collection).toArray().length, len, 'Test "union" in a Collection');
+
+    assert.equal(list.union([]).toArray().length, len, 'Test "union" with an empty array in a List');
+    assert.equal(list.union(collection).toArray().length, len, 'Test "union" in a List');
+
+    assert.equal(readOnlyCollection.union([]).toArray().length, len, 'Test "union" with an empty array in a ReadOnlyCollection');
+    assert.equal(readOnlyCollection.union(collection).toArray().length, len, 'Test "union" in a ReadOnlyCollection');
+
+    assert.equal(linkedList.union([]).toArray().length, len, 'Test "union" with an empty array in a LinkedList');
+    assert.equal(linkedList.union(collection).toArray().length, len, 'Test "union" in a LinkedList');
+
+    assert.equal(hashSet.union([]).toArray().length, len, 'Test "union" with an empty array in a HashSet');
+    assert.equal(hashSet.union(collection).toArray().length, len, 'Test "union" in a HashSet');
+
+    assert.equal(stack.union([]).toArray().length, len, 'Test "union" with an empty array in a Stack');
+    assert.equal(stack.union(collection).toArray().length, len, 'Test "union" in a Stack');
+
+    assert.equal(queue.union([]).toArray().length, len, 'Test "union" with an empty array in a Queue');
+    assert.equal(queue.union(collection).toArray().length, len, 'Test "union" in a Queue');
+
+    assert.equal(set.union([]).toArray().length, len, 'Test "union" with an empty array in a Set');
+    assert.equal(set.union(set).toArray().length, len, 'Test "union" in a Set');
+
+    assert.equal(map.union([]).toArray().length, len, 'Test "union" with an empty array in a Map');
+    assert.equal(map.union(map).toArray().length, 2 * len, 'Test "union" in a Map');
+
+    assert.equal(dictionary.union([]).toArray().length, len, 'Test "union" with an empty array in a Dictionary');
+    assert.equal(dictionary.union(dictionary).toArray().length, len, 'Test "union" in a Dictionary');
+
+    assert.equal(lookup.union([]).toArray().length, len, 'Test "union" with an empty array in a Lookup');
+    assert.equal(lookup.union(lookup).toArray().length, len, 'Test "union" in a Lookup');
+
+    assert.equal(sortedList.union([]).toArray().length, len, 'Test "union" with an empty array in a SortedList');
+    assert.equal(sortedList.union(sortedList).toArray().length, len, 'Test "union" in a SortedList');
+});
+
+
+qtest('union method validations', function (assert) {
+    assert.throws(function () {
+        mx([1]).union();
+    }, 'null input');
 });
 
 })));
--- test/unit/linq/union.js
+++ test/unit/linq/union.js
@@ -1,8 +1,63 @@
 import mx from '../../multiplex';
-import {qmodule, qtest} from '../../qunit';
+import * as mocks from './_mocks';
+import {
+    qmodule,
+    qtest
+} from '../../qunit';
 
 qmodule('linq-union');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+
+qtest('basic "union" test', function (assert) {
+    assert.deepEqual(mx([]).union([]).toArray(), [], 'union two empty arrays');
+    assert.deepEqual(mx([1, 2]).union([]).toArray(), [1, 2], 'union an array with empty array');
+    assert.deepEqual(mx([1, 2]).union([3, 4]).toArray(), [1, 2, 3, 4], 'union two arrays');
+    assert.deepEqual(mx([1, 2]).union([1, 2]).toArray(), [1, 2], 'union two identical arrays');
+});
+
+
+qtest('collections "union" method tests', function (assert) {
+    var len = mocks.array.length;
+    assert.equal(mocks.collection.union([]).toArray().length, len, 'Test "union" with an empty array in a Collection');
+    assert.equal(mocks.collection.union(mocks.collection).toArray().length, len, 'Test "union" in a Collection');
+
+    assert.equal(mocks.list.union([]).toArray().length, len, 'Test "union" with an empty array in a List');
+    assert.equal(mocks.list.union(mocks.collection).toArray().length, len, 'Test "union" in a List');
+
+    assert.equal(mocks.readOnlyCollection.union([]).toArray().length, len, 'Test "union" with an empty array in a ReadOnlyCollection');
+    assert.equal(mocks.readOnlyCollection.union(mocks.collection).toArray().length, len, 'Test "union" in a ReadOnlyCollection');
+
+    assert.equal(mocks.linkedList.union([]).toArray().length, len, 'Test "union" with an empty array in a LinkedList');
+    assert.equal(mocks.linkedList.union(mocks.collection).toArray().length, len, 'Test "union" in a LinkedList');
+
+    assert.equal(mocks.hashSet.union([]).toArray().length, len, 'Test "union" with an empty array in a HashSet');
+    assert.equal(mocks.hashSet.union(mocks.collection).toArray().length, len, 'Test "union" in a HashSet');
+
+    assert.equal(mocks.stack.union([]).toArray().length, len, 'Test "union" with an empty array in a Stack');
+    assert.equal(mocks.stack.union(mocks.collection).toArray().length, len, 'Test "union" in a Stack');
+
+    assert.equal(mocks.queue.union([]).toArray().length, len, 'Test "union" with an empty array in a Queue');
+    assert.equal(mocks.queue.union(mocks.collection).toArray().length, len, 'Test "union" in a Queue');
+
+    assert.equal(mocks.set.union([]).toArray().length, len, 'Test "union" with an empty array in a Set');
+    assert.equal(mocks.set.union(mocks.set).toArray().length, len, 'Test "union" in a Set');
+
+    assert.equal(mocks.map.union([]).toArray().length, len, 'Test "union" with an empty array in a Map');
+    assert.equal(mocks.map.union(mocks.map).toArray().length, 2 * len, 'Test "union" in a Map');
+
+    assert.equal(mocks.dictionary.union([]).toArray().length, len, 'Test "union" with an empty array in a Dictionary');
+    assert.equal(mocks.dictionary.union(mocks.dictionary).toArray().length, len, 'Test "union" in a Dictionary');
+
+    assert.equal(mocks.lookup.union([]).toArray().length, len, 'Test "union" with an empty array in a Lookup');
+    assert.equal(mocks.lookup.union(mocks.lookup).toArray().length, len, 'Test "union" in a Lookup');
+
+    assert.equal(mocks.sortedList.union([]).toArray().length, len, 'Test "union" with an empty array in a SortedList');
+    assert.equal(mocks.sortedList.union(mocks.sortedList).toArray().length, len, 'Test "union" in a SortedList');
+});
+
+
+qtest('union method validations', function (assert) {
+    assert.throws(function () {
+        mx([1]).union();
+    }, 'null input');
 });
