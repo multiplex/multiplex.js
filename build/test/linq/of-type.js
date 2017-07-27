de1af0c0ffe6420079270656a11c6f0376c95d54
kamyarnazeri <kamyar.nzr@gmail.com>
2017-07-27 17:46:04 -0400

    line 'ofType' method unit tests

--- build/test/linq/of-type.js
+++ build/test/linq/of-type.js
@@ -6,14 +6,53 @@
 
 mx = 'default' in mx ? mx['default'] : mx;
 
+var array = [1, 2, 3, 4, 5];
+var enumerable = mx.range(1, 5);
+var collection = new mx.Collection(array);
+var list = new mx.List(array);
+var linkedList = new mx.LinkedList(array);
+var hashSet = new mx.HashSet(array);
+var stack = new mx.Stack(array);
+var queue = new mx.Queue(array);
+var set = new mx.Set(array);
+var map = new mx.Map();
+var dictionary = new mx.Dictionary();
+var sortedList = new mx.SortedList();
+var readOnlyCollection = list.asReadOnly();
+var lookup = new mx.Lookup(array, function (t) {
+    return t;
+});
+
+for (var i = 0; i < array.length; i++) {
+    map.set(array[i], array[i]);
+    dictionary.set(array[i], array[i]);
+    sortedList.add(array[i], array[i]);
+}
+
 var qunit = typeof QUnit === 'undefined' ? require('qunitjs') : QUnit;
 var qmodule = qunit.module;
 var qtest = qunit.test;
 
 qmodule('linq-of-type');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+qtest('basic "of-type" test', function (assert) {
+    assert.equal(mx(array).ofType(Number).count(), array.length, 'ofType Number in an array of numbers');
+    assert.equal(mx(array).ofType(String).count(), 0, 'ofType String in an array of numbers');
+    assert.equal(mx('string').ofType(String).count(), 'string'.length, 'ofType String in an array of String');
+    assert.equal(mx('string').ofType(Number).count(), 0, 'ofType Number in an array of numbers');
+});
+
+qtest('collections "of-type" method tests', function (assert) {
+    var len = array.length;
+    assert.equal(mx(array).ofType(Number).count(), len, 'Test toList in an array');
+    assert.equal(enumerable.ofType(Number).count(), len, 'Test toList in an enumerable');
+    assert.equal(collection.ofType(Number).count(), len, 'Test toList in a Collection');
+    assert.equal(list.ofType(Number).count(), len, 'Test toList in a List');
+    assert.equal(readOnlyCollection.ofType(Number).count(), len, 'Test toList in a ReadOnlyCollection');
+    assert.equal(linkedList.ofType(Number).count(), len, 'Test toList in a LinkedList');
+    assert.equal(hashSet.ofType(Number).count(), len, 'Test toList in a HashSet');
+    assert.equal(stack.ofType(Number).count(), len, 'Test toList in a Stack');
+    assert.equal(queue.ofType(Number).count(), len, 'Test toList in a Queue');
 });
 
 })));
--- test/unit/linq/of-type.js
+++ test/unit/linq/of-type.js
@@ -1,8 +1,28 @@
 import mx from '../../multiplex';
-import {qmodule, qtest} from '../../qunit';
+import * as mocks from './_mocks';
+import {
+    qmodule,
+    qtest
+} from '../../qunit';
 
 qmodule('linq-of-type');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+qtest('basic "of-type" test', function (assert) {
+    assert.equal(mx(mocks.array).ofType(Number).count(), mocks.array.length, 'ofType Number in an array of numbers');
+    assert.equal(mx(mocks.array).ofType(String).count(), 0, 'ofType String in an array of numbers');
+    assert.equal(mx('string').ofType(String).count(), 'string'.length, 'ofType String in an array of String');
+    assert.equal(mx('string').ofType(Number).count(), 0, 'ofType Number in an array of numbers');
+});
+
+qtest('collections "of-type" method tests', function (assert) {
+    var len = mocks.array.length;
+    assert.equal(mx(mocks.array).ofType(Number).count(), len, 'Test toList in an array');
+    assert.equal(mocks.enumerable.ofType(Number).count(), len, 'Test toList in an enumerable');
+    assert.equal(mocks.collection.ofType(Number).count(), len, 'Test toList in a Collection');
+    assert.equal(mocks.list.ofType(Number).count(), len, 'Test toList in a List');
+    assert.equal(mocks.readOnlyCollection.ofType(Number).count(), len, 'Test toList in a ReadOnlyCollection');
+    assert.equal(mocks.linkedList.ofType(Number).count(), len, 'Test toList in a LinkedList');
+    assert.equal(mocks.hashSet.ofType(Number).count(), len, 'Test toList in a HashSet');
+    assert.equal(mocks.stack.ofType(Number).count(), len, 'Test toList in a Stack');
+    assert.equal(mocks.queue.ofType(Number).count(), len, 'Test toList in a Queue');
 });
