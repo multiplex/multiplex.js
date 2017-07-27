627380c4beffb7ee5ee8594db5d9133845c4747e
Kamyar Nazeri <kamyar.nzr@gmail.com>
2017-07-26 20:53:57 -0400

    linq 'range' unit tests

--- test/unit/linq/range.js
+++ test/unit/linq/range.js
@@ -4,31 +4,31 @@ import { qmodule, qtest } from '../../qunit';
 qmodule('linq-range');
 
 qtest('basic "range" tests', function (assert) {
-    assert.equal(mx.Iterable.range(0, 5).count(), 5, 'Test range of 5 numbers');
-    assert.equal(mx.Iterable.range(5, 0).count(), 0, 'Test empty range of numbers');
-    assert.deepEqual(mx.Iterable.range(0, 5).toArray(), [0, 1, 2, 3, 4], 'Test range value of 5 numbers');
-    assert.deepEqual(mx.Iterable.range(-5, 5).toArray(), [-5, -4, -3, -2, -1], 'Test range value of 5 negative numbers');
+    assert.equal(mx.range(0, 5).count(), 5, 'Test range of 5 numbers');
+    assert.equal(mx.range(5, 0).count(), 0, 'Test empty range of numbers');
+    assert.deepEqual(mx.range(0, 5).toArray(), [0, 1, 2, 3, 4], 'Test range value of 5 numbers');
+    assert.deepEqual(mx.range(-5, 5).toArray(), [-5, -4, -3, -2, -1], 'Test range value of 5 negative numbers');
 });
 
 
 qtest('range method validations', function (assert) {
     assert.throws(function () {
-        mx.Iterable.range();
+        mx.range();
     }, 'undefined start');
 
     assert.throws(function () {
-        mx.Iterable.range(1);
+        mx.range(1);
     }, 'undefined end');
 
     assert.throws(function () {
-        mx.Iterable.range(true, 5);
+        mx.range(true, 5);
     }, 'non-number start');
 
     assert.throws(function () {
-        mx.Iterable.range(5, true);
+        mx.range(5, true);
     }, 'non-number end');
 
     assert.throws(function () {
-        mx.Iterable.range(0, -5);
+        mx.range(0, -5);
     }, 'negative end');
 });
