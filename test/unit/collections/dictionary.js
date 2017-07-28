d9599f5b946ac7e069fb6be1f6115f193cadaf2d
Kamyar Nazeri <kamyar.nzr@gmail.com>
2017-07-27 22:14:05 -0400

    update dictionary tests

--- test/unit/collections/dictionary.js
+++ test/unit/collections/dictionary.js
@@ -6,7 +6,15 @@ qmodule('dictionary');
 var Dictionary = mx.Dictionary;
 
 qtest('create dictionary', function (assert) {
-    assert.ok(new Dictionary() !== null, 'empty dictionary');
+    var dic = new Dictionary();
+
+    dic.add('A', 1);
+    assert.ok(dic.get('A') === 1, 'dictionary add/get');
+
+    dic.remove('A');
+    assert.ok(dic.containsKey('A') === false, 'dictionary remove');
+
+    dic.clear();
 });
 
 
