ed20bd3989470eb19741ac460094a6494700fb5d
Kamyar Nazeri <kamyar.nzr@gmail.com>
2017-07-28 14:58:50 -0400

    sorted-list tests

--- build/test/collections/sorted-list.js
+++ build/test/collections/sorted-list.js
@@ -19,6 +19,31 @@ qtest('create sorted-list', function (assert) {
 });
 
 
+qtest('basic sorted-list tests', function (assert) {
+    var dic = mx([1, 2, 3, 4, 5]).toDictionary(function (t) {
+        return t;
+    });
+    var list = new SortedList(dic);
+
+    assert.ok(list.count() === 5, 'basic sorted-list test');
+    assert.ok(list.keys().length === 5, 'basic sorted-list test');
+    assert.ok(list.values().length === 5, 'basic sorted-list test');
+
+    list.get(1);
+    list.containsKey(1);
+    list.containsValue(1);
+    list.indexOfKey(1);
+    list.indexOfValue(1);
+    list.remove(1);
+    list.removeAt(0);
+    list.set(1, 1);
+    list.trimExcess();
+    list.insert(1, 1, 1);
+    list.tryGetValue(1, function () {
+    });
+    list.clear();
+});
+
 qtest('sorted-list toString', function (assert) {
     assert.equal(new SortedList().toString(), '[SortedList]', 'SortedList toString');
 });
