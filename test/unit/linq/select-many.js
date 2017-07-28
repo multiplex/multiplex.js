20357859cd31af00feeae7ae491a94b48a5fb149
Kamyar Nazeri <kamyar.nzr@gmail.com>
2017-07-27 22:37:59 -0400

    init missing tests

--- test/unit/linq/select-many.js
+++ test/unit/linq/select-many.js
@@ -1,8 +1,16 @@
 import mx from '../../multiplex';
-import {qmodule, qtest} from '../../qunit';
+import * as mocks from './_mocks';
+import {
+    qmodule,
+    qtest
+} from '../../qunit';
 
 qmodule('linq-select-many');
 
-qtest('dummy', function (assert) {
-    assert.ok(mx([]), 'dumy');
+function identity(t) {
+    return t;
+}
+
+qtest('basic select-many test', function (assert) {
+    assert.equal(mx([mocks.array]).selectMany(identity, identity).count(), mocks.array.length, 'Test selectMany numbers in an array are less than 10');
 });
